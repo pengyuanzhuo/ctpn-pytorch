7aa3d18ab99242c431275671faef65d916db01f4
pengyuanzhuo <pengyuanzhuo@qiniu.com>
2019-04-28 17:36:33 +0800

    init cmt

---
+++ net/ctpn.py
@@ -0,0 +1,76 @@
+# coding: utf-8
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from net.vgg import vgg16_bn
+
+
+class Im2col(nn.Module):
+    def __init__(self, kernel_size, stride, padding):
+        '''
+        conv2d without learnable parameters
+        '''
+        super(Im2col, self).__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+    def forward(self, x):
+        h = x.shape[2] # x.shape = (N, C, H, W)
+        x = F.unfold(x, self.kernel_size, padding=self.padding, stride=self.stride) # x.shape=(N, C*kernel_size, L)
+        x = x.reshape((x.shape[0], x.shape[1], h, -1)) # channel=C*kernel_size, shape=(N, FN, h, w)
+        return x
+
+
+class BLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, batch_first=True, bidirectional=True):
+        super(BLSTM, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=batch_first, bidirectional=bidirectional)
+
+    def forward(self, x):
+        x = x.permute((0, 2, 3, 1)) # (N, C, H, W) => (N, H, W, C)
+        N, H, W, C = x.size()
+        x = x.reshape((N*H, W, C)) # (N, H, W, C) => (NH, W, C)
+        x, _ = self.lstm(x) # shape=(batch, seq_len, num_directions*hidden_size)=(NH, W, 256)
+        x = x.reshape((N, H, W, x.shape[-1])) # (NH, W, 256) => (N, H, W, 256)
+        x = x.permute((0, 3, 1, 2))
+        # assert x.shape[1] == 256
+
+        return x
+
+
+class CTPN(nn.Module):
+    def __init__(self):
+        super(CTPN, self).__init__()
+        self.feature = vgg16_bn(True)
+        self.im2col = Im2col(3, 1, 1)
+        self.lstm = BLSTM(3*3*512, 128, True, True)
+
+        self.fc = nn.Conv2d(256, 512, 3, 1)
+
+        self.cls = nn.Conv2d(512, 2*10, kernel_size=1, stride=1)
+        self.loc = nn.Conv2d(512, 2*10, kernel_size=1, stride=1)
+        self.side_refinement = nn.Conv2d(512, 10, kernel_size=1, stride=1)
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.feature(x)
+        x = self.im2col(x)
+        x = self.lstm(x)
+        x = self.fc(x)
+        x = self.relu(x)
+
+        score = self.cls(x)
+        loc = self.loc(x)
+        side_refinement = self.side_refinement(x)
+
+        return score, loc, side_refinement
+
+
+if __name__ == '__main__':
+    model = CTPN()
+    print(model)
+
---
+++ net/loss.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+
+import torch
+import torch.nn as nn
+
+
+class CtpnLoss(nn.Module):
+    def __init__(self, lambd_reg=1.0):
+        super(CtpnLoss, self).__init__()
+        self.lambd_reg = lambd_reg
+        # self.lambd_refinement = 2.0
+        self.CrossEntropyLoss = nn.CrossEntropyLoss()
+        self.SmoothL1Loss = nn.SmoothL1Loss()
+
+    def forward(self, score, loc, score_target, loc_target):
+        '''
+        :param score: prediction score, shape=(1, 10*2, H, W)
+        :param loc: prediction loc, shape=(1, 10*2, H, W)
+        :param side_refinement:
+        :param score_target: gt anchor label from rpn target. shape=(1, 10, H, W)
+        :param loc_target: gt bbox regression target from rpn target. shape=(1, 40, H, W)
+        :param side_refinement_target
+        :return total loss
+        '''
+        # part1, classification loss
+        score = score.reshape((score.shape[0], 2, -1, score.shape[-1])) # shape=(1, 2, 10H, W)
+        score = score.permute((0, 3, 1, 2))
+        score = score.reshape((-1, 2)) # shape=(num_all_anchor, 2)
+
+        score_target = score_target.reshape((score_target.shape[0], 1, -1, score_target.shape[-1])) # shape=(1, 1, 10H, W)
+        score_target = score_target.permute((0, 3, 1, 2)) # (1, 10H, W, 1)
+        score_target = score_target.reshape((-1))
+
+        valid_indices = (score_target >= 0).nonzero()[:, 0] # positive & negative label indices
+        score = score[valid_indices, :]
+        score_target = score_target[valid_indices]
+
+        cls_loss = self.CrossEntropyLoss(score, score_target)
+
+        # part2, bbox regression loss
+        # normalized by (num of valid_samples)
+        # DIFFRENR FROM PAPER !!!
+        # paper: valid sample(positive or iou > 0.5 anchors)
+        # code: valid sample(positive anchors)
+        loc = loc.reshape((loc.shape[0], 2, -1, loc.shape[-1]))
+        loc = loc.permute((0, 3, 1, 2))
+        loc = loc.reshape((-1, 2))
+        loc = loc[valid_indices, :]
+
+        # loc_target = loc_target[:, (1, 3), :, :] # shape=(1, 20, H, W)
+        loc_target = loc_target.reshape((loc_target.shape[0], 2, -1, loc_target.shape[-1]))
+        loc_target = loc_target.permute((0, 3, 1, 2))
+        loc_target = loc_target.reshape((-1, 2))
+        loc_target = loc_target[valid_indices, :]
+
+        loc_loss = self.SmoothL1Loss(loc, loc_target)
+
+        # part3, side_refinement loss
+        # TODO
+
+        return cls_loss + self.lambd_reg * loc_loss
+
+if __name__ == '__main__':
+    loss = CtpnLoss()
+    score = torch.rand((1, 20, 32, 32))
+    score_target = torch.rand((1, 10, 32, 32))
+    score_target = (score_target > 0.5).long()
+    loc = torch.rand((1, 20, 32, 32))
+    loc_target = torch.rand((1, 20, 32, 32))
+    ls = loss(score, loc, score_target, loc_target)
+    print(ls)
+

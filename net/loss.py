f9d6b6846d0806b47a076eb94c9af3e14e5a0cfc
pengyuanzhuo <pengyuanzhuo@qiniu.com>
2019-05-08 09:59:29 +0800

    fix bug

--- net/loss.py
+++ net/loss.py
@@ -14,21 +14,21 @@ class CtpnLoss(nn.Module):
 
     def forward(self, score, loc, score_target, loc_target):
         '''
-        :param score: prediction score, shape=(1, 10*2, H, W)
-        :param loc: prediction loc, shape=(1, 10*2, H, W)
+        :param score: prediction score, shape=(N, 10*2, H, W)
+        :param loc: prediction loc, shape=(N, 10*2, H, W)
         :param side_refinement:
-        :param score_target: gt anchor label from rpn target. shape=(1, 10, H, W)
-        :param loc_target: gt bbox regression target from rpn target. shape=(1, 40, H, W)
+        :param score_target: gt anchor label from rpn target. shape=(N, 10, H, W)
+        :param loc_target: gt bbox regression target from rpn target. shape=(N, 20, H, W)
         :param side_refinement_target
         :return total loss
         '''
         # part1, classification loss
-        score = score.reshape((score.shape[0], 2, -1, score.shape[-1])) # shape=(1, 2, 10H, W)
-        score = score.permute((0, 3, 1, 2))
+        score = score.reshape((score.shape[0], 2, -1, score.shape[-1])) # shape=(N, 2, 10H, W)
+        score = score.permute((0, 2, 3, 1)) # shape=(N, 10H, W, 2)
         score = score.reshape((-1, 2)) # shape=(num_all_anchor, 2)
 
         score_target = score_target.reshape((score_target.shape[0], 1, -1, score_target.shape[-1])) # shape=(1, 1, 10H, W)
-        score_target = score_target.permute((0, 3, 1, 2)) # (1, 10H, W, 1)
+        score_target = score_target.permute((0, 2, 3, 1)) # (1, 10H, W, 1)
         score_target = score_target.reshape((-1))
 
         valid_indices = (score_target >= 0).nonzero()[:, 0] # positive & negative label indices
@@ -42,8 +42,8 @@ class CtpnLoss(nn.Module):
         # DIFFRENR FROM PAPER !!!
         # paper: valid sample(positive or iou > 0.5 anchors)
         # code: valid sample(positive anchors)
-        loc = loc.reshape((loc.shape[0], 2, -1, loc.shape[-1]))
-        loc = loc.permute((0, 3, 1, 2))
+        loc = loc.reshape((loc.shape[0], 2, -1, loc.shape[-1])) # (N, 2, 10H, W)
+        loc = loc.permute((0, 2, 3, 1)) # (N, 2, 10H, W)
         loc = loc.reshape((-1, 2))
         loc = loc[valid_indices, :]
 
@@ -52,6 +52,8 @@ class CtpnLoss(nn.Module):
         loc_target = loc_target.permute((0, 3, 1, 2))
         loc_target = loc_target.reshape((-1, 2))
         loc_target = loc_target[valid_indices, :]
+
+        assert loc.shape == loc_target.shape
 
         loc_loss = self.SmoothL1Loss(loc, loc_target)
 

58e9c4c3bede0c0e4a4296a0c5e31c1698b7d9dd
pengyuanzhuo <pengyuanzhuo@qiniu.com>
2019-05-08 09:56:26 +0800

    cfg

--- config.py
+++ config.py
@@ -3,6 +3,10 @@
 class TrainConfig:
     TRAIN_DIR = '/Users/qiniu/Workspace/private/ctpn-pytorch/dataset/vis' # 训练集目录
     GPUS = '0' # GPU列表(例如0,1,2,3)
+
     EPOCHS = 50
+    LR = 0.001
+    MOMENTUM = 0.9
+    WD = 0.0005 # weight decay
 
     LAMBD_REG = 1.0 # bbox regression weight, 默认1.0

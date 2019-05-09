3717885ce2cd6a9ad311a1687cb49fdaaf05dc7e
pengyuanzhuo <pengyuanzhuo@qiniu.com>
2019-05-09 11:47:02 +0800

    infer

--- demo.py
+++ demo.py
@@ -13,6 +13,7 @@ def preprocess(img, im_size):
     h, w, _ = img.shape
     scale = float(im_size) / float(min(h, w))
     img = cv2.resize(img, (0, 0), fx=scale, fy=scale)
+    h_new, w_new, _ = img.shape
     img = np.ascontiguousarray(img[:, :, ::-1])
     img = img.transpose((2, 0, 1))
     img = torch.from_numpy(img).float() / 255.0
@@ -21,17 +22,25 @@ def preprocess(img, im_size):
     img -= mean[:, None, None]
     img /= std[:, None, None]
 
-    return img[None, :, :, :]
+    im_info = (h_new, w_new, scale)
+
+    return img[None, :, :, :], im_info
 
 
-def infer(input, model):
-    pass
+def infer(inputs, im_info, model):
+    model.eval()
+    with torch.no_grad():
+        cls_map, reg_map = model(inputs)
+        scores, proposal = bbox_proposal(cls_map.numpy(), reg_map.numpy(),
+                                         im_info, feat_stride=16)
+
+    return scores, proposal
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('img', type=str, help='image path')
-    parser.add_argument('model', type=str, help='model path')
+    parser.add_argument('checkpoint', type=str, help='checkpoint path')
     return parser.parse_args()
 
 
@@ -39,15 +48,14 @@ if __name__ == '__main__':
     args = parse_args()
 
     img = cv2.imread(args.img)
-    inputs = preprocess(img, 768)
+    inputs, im_info = preprocess(img, 768)
 
-    print(inputs.shape)
+    checkpoint = torch.load(args.checkpoint)
+    model = CTPN()
+    model.load_state_dict(checkpoint['model_state_dict'])
 
-    # load model
-    # model = TODO
-    # model.eval()
-
-    # out = infer(inputs, model)
-
+    scores, proposal = infer(inputs, im_info, model)
+    print(scores.shape)
+    print(proposal.shape)
     # draw
     # TODO

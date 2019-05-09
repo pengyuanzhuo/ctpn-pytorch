c84d663c839973efec03d4ab91ec6f575b5c3020
pengyuanzhuo <pengyuanzhuo@qiniu.com>
2019-05-09 11:32:19 +0800

    cfg

--- config.py
+++ config.py
@@ -2,7 +2,13 @@
 
 class TrainConfig:
     TRAIN_DIR = '/Users/qiniu/Workspace/private/ctpn-pytorch/dataset/vis' # 训练集目录
+    VAL_DIR = None # 验证集目录
     GPUS = '0' # GPU列表(例如0,1,2,3)
+    CHECKPOINT_DIR = './checkpoints' # checkpoint dir
+    CHECKPOINT = None # 从checkpoint 恢复训练
+    PRINT_FREQ = 20
+
+    FEAT_STRIDE = 16 # vgg16
 
     EPOCHS = 50
     LR = 0.001
